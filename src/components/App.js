5e17335293a6f7a5166178cccdb94ac353c83887
tommy-atkins <thomas.robert.atkins@gmail.com>
2022-02-01 19:10:51 +0800

    responsive changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Nav from './components/Nav';
+import Nav from './Nav';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { GlobalStyle } from "./styles/GlobalStyle";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Main from "./components/Main";
-import ErrorPage from "./components/ErrorPage";
+import { GlobalStyle } from "../styles/GlobalStyle";
+import Projects from "./Projects";
+import Skills from "./Skills";
+import Main from "./Main";
+import ErrorPage from "./ErrorPage";
 
 function App() {
   return (
