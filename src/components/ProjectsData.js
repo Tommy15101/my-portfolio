2fc9c95435e20b3bb952d37339f4ad7b81d70dc4
tommy-atkins <thomas.robert.atkins@gmail.com>
2022-11-23 18:19:16 +0700

    updates

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -1,69 +1,69 @@
-import exchange from '../images/exchange.png';
-import cryptoprices from '../images/crypto_prices.png';
-import leonardo from '../images/leaonardo-teaser.png';
+import exchange from "../images/exchange.png";
+import cryptoprices from "../images/crypto_prices.png";
+import leonardo from "../images/leaonardo-teaser.png";
 // import bestofbali from '../images/bestofbali.png';
 
 export const ProjectsData = {
-    projects:[
-        {
-            id: 1,
-            imgSrc: exchange,
-            title: 'Crypto Exchange',
-            desc: 'Completed with Dapp University',
-            info: 'Deployed to the Rinkeby Test Network, this is a fully functioning ERC20 token and exchange.',
-            skillsHeader: 'Key Skills',
-            skillsOne: 'Solidity',
-            skillsTwo: 'React',
-            skillsThree: 'Redux',
-            buttonOne: 'View Code',
-            buttonTwo: 'Visit Exchange',
-            code: 'https://github.com/Tommy15101/CapstoneProject',
-            visit: 'https://bali-token-exchange.on.fleek.co'
-        },
-        {
-            id: 2,
-            imgSrc: cryptoprices,
-            title: 'Crypto Price Tracker',
-            desc: 'Top 100 Crypto Assets Live Prices',
-            info: 'A simple CryptoCurrencey price tracker with data sourced from the Coin Gecko API.',
-            skillsHeader: 'Key Skills',
-            skillsOne: 'React',
-            skillsTwo: 'CSS',
-            skillsThree: 'JS',
-            buttonOne: 'View Code',
-            buttonTwo: 'Visit Tracker',
-            code: 'https://github.com/Tommy15101',
-            visit: 'https://crypto-prices.on.fleek.co'
-        },
-        {
-            id: 3,
-            imgSrc: leonardo,
-            title: 'NFT Collection',
-            desc: 'In Progress - Coming Soon',
-            info: 'A collection of 20 unique pieces',
-            skillsHeader: 'Key Skills',
-            skillsOne: 'Solidity',
-            skillsTwo: 'React',
-            skillsThree: 'IPFS',
-            buttonOne: 'Coming Soon',
-            buttonTwo: 'Coming Soon',
-            // code: 'https://github.com/Tommy15101',
-            // visit: 'https://flat-credit-3558.on.fleek.co/'
-        },
-        // {
-        //     id: 4,
-        //     imgSrc: bestofbali,
-        //     title: 'Best of Bali',
-        //     desc: 'In Progress - Coming Soon',
-        //     info: 'Discover the best of everything Bali has to offer. Share you experiences. The Bali expat inner circle',
-        //     skillsHeader: 'Key Skills',
-        //     skillsOne: 'GraphQL',
-        //     skillsTwo: 'React',
-        //     skillsThree: 'CSS',
-        //     buttonOne: 'View Code',
-        //     buttonTwo: 'Coming Soon',
-        //     code: 'https://github.com/Tommy15101/best-of-bali',
-        //     // visit: 'https://flat-credit-3558.on.fleek.co/'
-        // }
-    ]
-}
+  projects: [
+    {
+      id: 1,
+      imgSrc: exchange,
+      title: "Crypto Exchange",
+      desc: "Completed with Dapp University",
+      info: "Please Select Mumbai test nextwork to interact",
+      skillsHeader: "Key Skills",
+      skillsOne: "Solidity",
+      skillsTwo: "React",
+      skillsThree: "Redux",
+      buttonOne: "View Code",
+      buttonTwo: "Visit Exchange",
+      code: "https://github.com/Tommy15101/digital-nomad-exchange",
+      visit: "https://broad-dust-9340.on.fleek.co/",
+    },
+    {
+      id: 2,
+      imgSrc: cryptoprices,
+      title: "Crypto Price Tracker",
+      desc: "Top 100 Crypto Assets Live Prices",
+      info: "A simple CryptoCurrencey price tracker with data sourced from the Coin Gecko API.",
+      skillsHeader: "Key Skills",
+      skillsOne: "React",
+      skillsTwo: "CSS",
+      skillsThree: "JS",
+      buttonOne: "View Code",
+      buttonTwo: "Visit Tracker",
+      code: "https://github.com/Tommy15101",
+      visit: "https://crypto-prices.on.fleek.co",
+    },
+    {
+      id: 3,
+      imgSrc: leonardo,
+      title: "NFT Collection",
+      desc: "In Progress - Coming Soon",
+      info: "A collection of 20 unique pieces",
+      skillsHeader: "Key Skills",
+      skillsOne: "Solidity",
+      skillsTwo: "React",
+      skillsThree: "IPFS",
+      buttonOne: "Coming Soon",
+      buttonTwo: "Coming Soon",
+      // code: 'https://github.com/Tommy15101',
+      // visit: 'https://flat-credit-3558.on.fleek.co/'
+    },
+    // {
+    //     id: 4,
+    //     imgSrc: bestofbali,
+    //     title: 'Best of Bali',
+    //     desc: 'In Progress - Coming Soon',
+    //     info: 'Discover the best of everything Bali has to offer. Share you experiences. The Bali expat inner circle',
+    //     skillsHeader: 'Key Skills',
+    //     skillsOne: 'GraphQL',
+    //     skillsTwo: 'React',
+    //     skillsThree: 'CSS',
+    //     buttonOne: 'View Code',
+    //     buttonTwo: 'Coming Soon',
+    //     code: 'https://github.com/Tommy15101/best-of-bali',
+    //     // visit: 'https://flat-credit-3558.on.fleek.co/'
+    // }
+  ],
+};
